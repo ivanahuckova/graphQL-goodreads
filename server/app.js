08c85661ed5641ed6953c51bd10def33b1a216b8
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-08 21:31:04 +0200

    import and add schema

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({ schema }));
 
 const port = 4000;
 
