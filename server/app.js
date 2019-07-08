ec1e5ec3194c9aeae254fc007a83d56a93a256d8
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-08 22:00:32 +0200

    create book and author types and add to root query

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const schema = require('./schema/schema');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({ schema }));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
 const port = 4000;
 
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,27 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt } = graphql;
+
+// dummy data
+const books = [{ name: 'Hello', genre: 'comedy', id: '1' }, { name: 'Hello2', genre: 'comedy', id: '2' }, { name: 'Hello3', genre: 'fantasy', id: '3' }];
+const authors = [{ name: 'Author1', age: 44, id: '1' }, { name: 'Author2', age: 47, id: '2' }, { name: 'Author3', age: 45, id: '3' }];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString },
         genre: { type: GraphQLString }
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: { type: GraphQLID },
+        age: { type: GraphQLInt },
+        name: { type: GraphQLString }
     })
 });
 
@@ -16,9 +30,17 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: { type: GraphQLString } },
+            args: { id: { type: GraphQLID } },
             resolve(parent, args) {
                 //code to get data from db / other source
+                return _.find(books, { id: args.id });
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return _.find(authors, { id: args.id });
             }
         }
     }
