4c1472e9bb56d14d33dbc4e5ca59fc4487bf827f
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-08 23:08:13 +0200

    update authorId type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -90,7 +90,7 @@ const Mutation = new GraphQLObjectType({
             args: {
                 name: { type: GraphQLString },
                 genre: { type: GraphQLString },
-                authorId: { type: GraphQLString }
+                authorId: { type: GraphQLID }
             },
             resolve(parent, args) {
                 let book = new Book({
