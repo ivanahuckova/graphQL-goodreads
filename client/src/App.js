fb2de0bb4dfd4cc35466130fea4d9c9f335194c1
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-09 14:20:18 +0200

    add addbook functionality

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from 'react-apollo';
 
 // import components
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
 
 // apollo client setup
 const client = new ApolloClient({
@@ -16,6 +17,7 @@ function App() {
             <div id="main">
                 <h1>Books I enjoyed in 2019</h1>
                 <BookList />
+                <AddBook />
             </div>
         </ApolloProvider>
     );
---
+++ client/src/components/AddBook.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
+
+const getAuthorsQuery = gql`
+    {
+        authors {
+            name
+            id
+        }
+    }
+`;
+
+class AddBook extends Component {
+    displayAuthors() {
+        let data = this.props.data;
+        if (data.loading) {
+            return <option disabled>Loading authors...</option>;
+        } else {
+            return data.authors.map(author => {
+                return <option key={author.id}>{author.name}</option>;
+            });
+        }
+    }
+    render() {
+        return (
+            <div>
+                <form id="add-book">
+                    <div className="field">
+                        <label>Book name:</label>
+                        <input type="text" />
+                    </div>
+
+                    <div className="field">
+                        <label>Genre:</label>
+                        <input type="text" />
+                    </div>
+
+                    <div className="field">
+                        <label>Author:</label>
+                        <select>
+                            <option>Select author</option>
+                            {this.displayAuthors()}
+                        </select>
+                    </div>
+
+                    <button>+</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default graphql(getAuthorsQuery)(AddBook);
