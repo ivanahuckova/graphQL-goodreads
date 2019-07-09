d74cee39ed0bb8240a213b73f276ef4086d5328c
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-09 13:26:51 +0200

    create components structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+// import components
+import BookList from './components/BookList';
 function App() {
     return (
         <div id="main">
             <h1>Books I enjoyed in 2019</h1>
+            <BookList />
         </div>
     );
 }
