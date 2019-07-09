2b995ed1583b7ecd8424cd730c0eaa599f7762eb
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-09 13:23:11 +0200

    react app setup

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div id="main">
+            <h1>Books I enjoyed in 2019</h1>
+        </div>
+    );
+}
+
+export default App;
