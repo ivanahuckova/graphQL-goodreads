18578b120853c9e245561978708cfba8268131b8
Ivana Huckova <ivana.huckova@gmail.com>
2019-07-09 13:54:36 +0200

    create function to fetch and display books

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -12,12 +12,20 @@ const getBooksQuery = gql`
 `;
 
 class BookList extends Component {
+    displayBooks() {
+        let data = this.props.data;
+        if (data.loading) {
+            return <div>Loading books...</div>;
+        } else {
+            return data.books.map(book => {
+                return <li key={book.id}>{book.name}</li>;
+            });
+        }
+    }
     render() {
         return (
             <div>
-                <ul id="book-list">
-                    <li>Book name</li>
-                </ul>
+                <ul id="book-list">{this.displayBooks()}</ul>
             </div>
         );
     }
